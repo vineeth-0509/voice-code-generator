026ecfdd5e138be3f3d75edc39847641968f3592
Vineeth-0509 <vineeththungani@gmail.com>
2026-03-26 13:30:05 +0530

    filerversion for the new files from the ai overwritign the oldeversion of the code

--- src/services/ai-update.service.ts
+++ src/services/ai-update.service.ts
@@ -10,21 +10,21 @@ export const updateFilesWithAI = async (
   projectId: string,
   userPrompt: string,
 ) => {
-    try{
-  const files = await prisma.file.findMany({
-    where: { projectId },
-  });
-  if(!files || files.length == 0){
-    throw new Error("No files found for this project")
-  }
-  const fileText = files.map((f) => `${f.name}:\n${f.content}`).join("\n\n");
+  try {
+    const files = await prisma.file.findMany({
+      where: { projectId },
+    });
+    if (!files || files.length == 0) {
+      throw new Error("No files found for this project");
+    }
+    const fileText = files.map((f) => `${f.name}:\n${f.content}`).join("\n\n");
 
-  const completion = await openai.chat.completions.create({
-    model: "google/gemma-2-9b-it",
-    messages: [
-      {
-        role: "user",
-        content: `
+    const completion = await openai.chat.completions.create({
+      model: "google/gemma-2-9b-it",
+      messages: [
+        {
+          role: "user",
+          content: `
                 You are a senior React developer.
 
 You will be given project files.
@@ -55,22 +55,22 @@ ${userPrompt}
 Files:
 ${fileText}
                 `,
-      },
-    ],
-    temperature: 0,
-    max_tokens: 1500,
-  });
+        },
+      ],
+      temperature: 0,
+      max_tokens: 1500,
+    });
 
-  let text = completion.choices?.[0]?.message?.content ?? "";
-  if(!text){
-    throw new Error("Empty AI Response");
-  }
-  console.log('raw response:\n', text);
-  text = text.replace(/```json|```/g, "").trim();
+    let text = completion.choices?.[0]?.message?.content ?? "";
+    if (!text) {
+      throw new Error("Empty AI Response");
+    }
+    console.log("raw response:\n", text);
+    text = text.replace(/```json|```/g, "").trim();
 
-  const match = text.match(/\{[\s\S]*\}/);
-  if (!match) throw new Error("Invalid AI response");
-  let parsed;
+    const match = text.match(/\{[\s\S]*\}/);
+    if (!match) throw new Error("Invalid AI response");
+    let parsed;
     try {
       parsed = JSON.parse(match[0]);
     } catch (err) {
@@ -87,24 +87,30 @@ ${fileText}
     if (!parsed.files) {
       throw new Error("AI response missing 'files' field");
     }
-  const updates = [];
-  for (const [fileName, content] of Object.entries(parsed.files)) {
-    const file = files.find((f) => f.name === fileName);
-    if (!file) continue;
-    const updated = await prisma.file.update({
-      where: {
-        id: file.id,
-      },
-      data: {
-        content: content as string,
-      },
-    });
-    updates.push(updated);
-  }
-  return updates;
-} 
-catch(error: any){
+    const updates = [];
+    for (const [fileName, content] of Object.entries(parsed.files)) {
+      const file = files.find((f) => f.name === fileName);
+      if (!file) continue;
+
+      await prisma.fileVersion.create({
+        data: {
+          fileId: file.id,
+          content: file.content,
+        },
+      });
+      const updated = await prisma.file.update({
+        where: {
+          id: file.id,
+        },
+        data: {
+          content: content as string,
+        },
+      });
+      updates.push(updated);
+    }
+    return updates;
+  } catch (error: any) {
     console.error("AI UPDATE Error", error);
     throw error;
-}
-}
+  }
+};
